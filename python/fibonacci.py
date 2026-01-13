3bc4a947ffd093149591e9dd564e353acf0ac78f
GrahamBrown-OregonState <browgrah@oregonstate.edu>
2026-01-12 20:49:31 -0800

    xRevert "remove unnecessary"
    This reverts commit f283701e50622034e1868cfcd47bb923222faf4c.

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,3 +1,12 @@
+def findFib(n):    
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return findFib(n-2) + findFib(n-1)
+
+
 def memoFib(n):
     if n == 0:
         return []
